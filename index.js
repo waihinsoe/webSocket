73ade98fae168df14ab0e1cfc966cd777261f47d
waihinsoe <waihinsoe332004@gmail.com>
2023-08-08 23:01:41 +0630

    feat: chat app ui

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const socket = require("socket.io");
+/**__app setup__**/
+const app = express();
+
+/**__server setup__**/
+const server = app.listen(4000, () => {
+  console.log("app is listening at port 4000");
+});
+
+/**__route setup__**/
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+/**__socket setup__**/
+const io = socket(server);
+io.on("connection", (socket) => {
+  console.log("socket connection connected", socket.id);
+});
